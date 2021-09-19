8aa359917b4ec1cd4af045e0069f7d19c0e824a5
MSSangeetha <sangeethasanthosh.m@gmail.com>
2021-09-19 16:03:21 -0500

    studio for model validation

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -4,27 +4,33 @@ import org.launchcode.spaday.data.UserData;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping
-    public String displayAllUsers(Model model){
+    public String displayAllUsers(Model model) {
         model.addAttribute("title", "All Users");
-    model.addAttribute("users", UserData.getAll());
+        model.addAttribute("users", UserData.getAll());
         return "user/index";
     }
 
     //Handler method to render the form
     @GetMapping("add")
-    public String displayAddUserForm(Model model,@ModelAttribute User user){
+//    public String displayAddUserForm(Model model, @ModelAttribute User user) {
+        public String displayAddUserForm(Model model) {
 //        model.addAttribute("users", new User());
-        model.addAttribute("users", user);
+//        model.addAttribute("users", user);
+        model.addAttribute(new User());
         return "user/add";
 //    public String displayAddUserForm(@ModelAttribute User users){
 //        UserData.add(users);
@@ -32,27 +38,31 @@ public class UserController {
     }
 
     @PostMapping("add")
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        // add form submission handling code here
-        if(verify.equals(user.getPassword())){
+    public String processAddUserForm(@ModelAttribute @Valid User user, Errors errors,Model model, String verify) {
+        if (! verify.equals(user.getPassword())) {
+            model.addAttribute("errormessage", "no");
+//            model.addAttribute("page", "Viv Day Spa :: Create Your Account");
+            return "user/add";
+        } else if (errors.hasErrors()) {
+//            model.addAttribute("page", "Viv Day Spa :: Create Your Account");
+            return "user/add";
+        } else {
             UserData.add(user);
-            model.addAttribute("username",user.getUsername());
-            model.addAttribute("email",user.getEmail());
+            model.addAttribute("user",user);
+            model.addAttribute("users", UserData.getAll());
+//            model.addAttribute("page", "Viv Day Spa :: Welcome");
             return "redirect:";
         }
-        else{
-            model.addAttribute("username",user.getUsername());
-            model.addAttribute("email",user.getEmail());
-            model.addAttribute("errormessage","no");
-            return "user/add";
+
         }
-//        return "redirect:";
-    }
-    //Submit the form
+
+
+        //Submit the form
 //    @PostMapping("user")
 //    public String createEvent(@ModelAttribute User newUser){
 //        UserData.add(newUser);
 //        return "redirect:"; //redirects to the homepage (/events)
 //    }
+
 
 }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,28 +1,45 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class User {
 //fields
 private int id;
     private static int nextId = 0;
+
+    @NotBlank(message="UserName cannot be blank")
+    @Size(min=5,max=15,message="Username should be between 5 and 15 characters")
     private String username;
+
+    @Email(message = "Invalid Email. Try Again!")
     private String email;
+
+    @NotBlank(message="Password cannot be blank")
+    @Size(min=6,message="Password should be minimum of 6 characters")
     private String password;
 
+//    @NotBlank(message="Verify Password cannot be blank")
+//    @NotNull(message="Passwords do not match")
+//    private String verifyPassword;
+
     //Constructors
-//    public User(){
-//        this.id = nextId;
-//        nextId++;
-//    }
+    public User(){
+        this.id = nextId;
+        nextId++;
+    }
 
     public User(String id,String username, String email, String password) {
-//        this();
-        this.id = nextId;
+        this();
+//        this.id = nextId;
         this.username = username;
         this.email = email;
         this.password = password;
-        nextId++;
+//        this.verifyPassword = verifyPassword;
+//        nextId++;
     }
 
     //Getters and Setters
@@ -52,7 +69,17 @@ private int id;
 
     public void setPassword(String password) {
         this.password = password;
+//        this.checkPassword();
     }
+
+//    public String getVerifyPassword() {
+//        return verifyPassword;
+//    }
+
+//    public void setVerifyPassword(String verify) {
+//        this.verifyPassword = verifyPassword;
+////        this.checkPassword();
+//    }
 
     //toString
 
@@ -79,4 +106,12 @@ private int id;
     public int hashCode() {
         return Objects.hash(username);
     }
+
+    //Setup a new method to verify password
+//    private void checkPassword(){
+//        if((this.password != null) && (this.verifyPassword != null) && (this.password != this.verifyPassword)){
+//            this.verifyPassword = null;
+//        }
+//
+//    }
 }
